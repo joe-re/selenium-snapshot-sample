4fe66b0a16ca50907068fe4cb76b80ac1b5b514b
joe-re <joe.tialtngo@gmail.com>
2015-01-11 00:43:35 +0900

    loginしていない場合ははじかれるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/show'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +58,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root to: 'home#index'
 end
