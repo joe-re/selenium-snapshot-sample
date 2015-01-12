70c60c7332c15de8be117ab5c93e1e7d406858a5
joe-re <joe.tialtngo@gmail.com>
2015-01-12 13:13:06 +0900

    snapshotを撮るスクリプトを作成

---
+++ shot.rb
@@ -0,0 +1,25 @@
+require 'selenium-webdriver'
+
+driver = Selenium::WebDriver.for :firefox
+FileUtils.mkdir_p 'snapshots'
+
+[
+  { name: 'yui', email: 'yui_hirasawa@test.co.jp', password: 'yuihirasawa' },
+].each do |user|
+  # ログイン
+  driver.navigate.to 'http://localhost:3000'
+  driver.find_element(:id, 'user_email').send_keys(user[:email])
+  driver.find_element(:id, 'user_password').send_keys(user[:password])
+  driver.find_element(:name, 'commit').submit
+  sleep(1) # デモのため少し待機
+
+  # profileページに遷移
+  driver.navigate.to 'http://localhost:3000/profile'
+  sleep(1) # デモのため少し待機
+
+  # snapshotを撮る
+  driver.save_screenshot("snapshots/#{user[:name]}.png")
+
+  # 終了
+  driver.quit
+end
